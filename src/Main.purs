53d51936aa4cca2723259a4a902fc09046c46e5b
bouzuya <m@bouzuya.net>
2018-10-16 12:29:19 +0900

    read RouteConfig

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,24 @@ module Main
   ( main
   ) where
 
+import Data.Array as Array
+import Data.Maybe (maybe)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit)
+import Effect.Exception (throw)
+import Node.Encoding as Encoding
+import Node.FS.Sync as FS
+import Node.Path (FilePath)
+import Node.Process as Process
+import Prelude (Unit, bind, map, pure, show)
+import RouteConfig as RouteConfig
+
+read :: FilePath -> Effect RouteConfig.RouteConfig
+read p = map RouteConfig.fromString (FS.readTextFile Encoding.UTF8 p)
 
 main :: Effect Unit
-main = log "Hello"
+main = do
+  argv <- Process.argv
+  file <- maybe (throw "no arg") pure (argv Array.!! 1)
+  config <- read file
+  log (show config)
