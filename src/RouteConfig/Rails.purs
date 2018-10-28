38fce856204e7cdcf295e253be6df11ff4bdc41b
bouzuya <m@bouzuya.net>
2018-10-28 22:58:40 +0900

    delete RouteConfig

--- src/RouteConfig/Rails.purs
+++ src/RouteConfig/Rails.purs
@@ -13,22 +13,30 @@ import Data.String.Regex.Flags (noFlags)
 import PathTemplate as PathParameter
 import PathTemplate as PathTemplate
 import Prelude (bind, const, map, pure)
-import RouteConfig (Route, RouteConfig)
 import YAS (YAS)
 import YAS as YAS
 
-routeFromLine :: String -> Maybe Route
+routeFromLine :: String -> Maybe YAS.Route
 routeFromLine line = do
   let p = "^\\s*(delete|get|patch|post|put)\\s+'([^']+)'\\s*,\\s*to:\\s*'([^']+)'.*$"
   r <- either (const Nothing) Just (Regex.regex p noFlags)
   matches <- Regex.match r line
   case (NonEmptyArray.toArray matches) of
     [_, method', path', to'] -> do
-      method <- method'
+      pattern <- YAS.patternFromString "^[^/]+$" -- TODO
+      methodString <- method'
+      method <- Method.fromString (String.toUpper methodString)
       pathString <- path'
       path <- PathTemplate.fromConfigString pathString
-      to <- to'
-      pure { method, path, to }
+      action <- to'
+      pure
+        { action
+        , method
+        , name: action
+        , parameters:
+            map (\name -> { name, pattern }) (PathParameter.parameterNames path)
+        , path
+        }
     _ -> Nothing
 
 fromString :: String -> YAS
@@ -36,24 +44,6 @@ fromString s =
   let
     lines = String.split (String.Pattern "\n") s
     routes = Array.catMaybes (map routeFromLine lines)
-  in
-    configToYAS { routes }
-
-configToYAS :: RouteConfig -> YAS
-configToYAS config =
-  let
-    toRoute r = do
-      pattern <- YAS.patternFromString "^[^/]+$" -- TODO
-      method <- Method.fromString (String.toUpper r.method)
-      pure
-        { action: r.to
-        , method
-        , name: r.to
-        , parameters:
-            map (\name -> { name, pattern }) (PathParameter.parameterNames r.path)
-        , path: r.path
-        }
-    routes = Array.catMaybes (map toRoute config.routes)
   in
     { actions: [] -- TODO
     , routes
