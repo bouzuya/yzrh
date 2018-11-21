3bffe63b6f8b9a1bb2919aef1f9dbc0f508d1c82
bouzuya <m@bouzuya.net>
2018-11-22 00:02:34 +0900

    replace getOption* with parseOption

--- src/CommandLineOption/OptionObject.purs
+++ src/CommandLineOption/OptionObject.purs
@@ -13,7 +13,7 @@ import Data.String as String
 import Data.String.CodeUnits as CodeUnit
 import Foreign.Object (Object)
 import Foreign.Object as Object
-import Prelude (const, map, notEq, (<<<), (==))
+import Prelude (const, map, (==))
 
 data OptionName = Long String | Short Char
 
@@ -32,32 +32,30 @@ findByOptionName name =
     Long l -> Array.find (\d -> getLongName d == l)
     Short s -> Array.find (\d -> getShortName d == Just s)
 
-getOptionName :: String -> Maybe OptionName
-getOptionName s =
+parseOption :: String -> { name :: Maybe OptionName, value :: Maybe String }
+parseOption s =
   let
-    p = String.Pattern "-"
-    v = String.takeWhile (notEq (String.codePointFromChar '='))
+    hyphen = String.Pattern "-"
+    -- "foo=bar" -> { name: "foo", value: "bar" }
+    split s' =
+      case String.indexOf (String.Pattern "=") s' of
+        Nothing -> { name: s', value: "" }
+        Just index ->
+          let { after, before } = String.splitAt index s'
+          in { name: before, value: (String.drop 1 after) }
+    nullToNothing s' = if String.null s' then Nothing else Just s'
   in
-    case String.stripPrefix p s of
+    case String.stripPrefix hyphen s of
+      Nothing -> { name: Nothing, value: Nothing }
       Just s' ->
-        case String.stripPrefix p s' of
-          Just s'' -> Just (Long (v s''))
-          _ -> map Short (CodeUnit.charAt 0 (v s')) -- TODO: -fg
-      Nothing -> Nothing
+        case String.stripPrefix hyphen s' of
+          Just s'' ->
+            let { name, value } = split s''
+            in { name: Just (Long name), value: nullToNothing value }
+          _ ->
+            let { name, value } = split s'
+            in { name: map Short (CodeUnit.charAt 0 name), value: nullToNothing value } -- TODO: -fg
 
-getOptionValue :: String -> Maybe String
-getOptionValue s =
-  let
-    p = String.Pattern "-"
-    v = (String.drop 1) <<< (String.dropWhile (notEq (String.codePointFromChar '=')))
-  in
-    case String.stripPrefix p s of
-      Just s' ->
-        let s'' = v s'
-        in if String.null s'' then Nothing else Just s''
-      Nothing -> Nothing
-
--- ["--name", "value"] -> { name: "value" }
 toObject :: Array OptionDefinition -> Array String -> OptionObject
 toObject defs options =
   let
@@ -72,36 +70,40 @@ toObject defs options =
       Just _ -> parsed.parsed -- ERROR: no metavar (end)
   where
     f { processing: Nothing, parsed } s =
-      case getOptionName s of
-        Nothing ->
-          { parsed
-          , processing: Nothing
-          } -- ERROR: arguments is not supported
-        Just name ->
-          -- TODO: long == "" -- double hyphen (--) support
-          case findByOptionName name defs of
-            Nothing ->
-              { parsed
-              , processing: Nothing
-              } -- ERROR: unknown option
-            Just def ->
-              let
-                long = getLongName def
-              in
-                if isValueRequired def then
-                  case getOptionValue s of
-                    Just value ->
-                      { parsed: Object.insert long (OptionValue.fromString value) parsed
+      let { name: nameMaybe, value: valueMaybe } = parseOption s
+      in
+        case nameMaybe of
+          Nothing ->
+            { parsed
+            , processing: Nothing
+            } -- ERROR: arguments is not supported
+          Just name ->
+            -- TODO: long == "" -- double hyphen (--) support
+            case findByOptionName name defs of
+              Nothing ->
+                { parsed
+                , processing: Nothing
+                } -- ERROR: unknown option
+              Just def ->
+                case valueMaybe of
+                  Just value ->
+                    if isValueRequired def then
+                      { parsed: Object.insert (getLongName def) (OptionValue.fromString value) parsed
                       , processing: Nothing
                       }
-                    Nothing ->
+                    else
+                      { parsed
+                      , processing: Nothing
+                      } -- ERROR: boolean option can't specify value.
+                  Nothing ->
+                    if isValueRequired def then
                       { parsed
                       , processing: Just def
                       }
-                else
-                  { parsed: Object.insert long (OptionValue.fromBoolean true) parsed
-                  , processing: Nothing
-                  }
+                    else
+                      { parsed: Object.insert (getLongName def) (OptionValue.fromBoolean true) parsed
+                      , processing: Nothing
+                      }
     f { parsed, processing: Just def } s =
       -- TODO: value == "--foo" -- no metavar (next option)
       { parsed: Object.insert (getLongName def) (OptionValue.fromString s) parsed
