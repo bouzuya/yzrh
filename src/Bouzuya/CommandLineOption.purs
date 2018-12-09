b0fc7a063933da8b7370dc01c37c1bed6bc95897
bouzuya <m@bouzuya.net>
2018-12-09 21:28:18 +0900

    fix type error

--- src/Bouzuya/CommandLineOption.purs
+++ src/Bouzuya/CommandLineOption.purs
@@ -1,27 +1,35 @@
 module Bouzuya.CommandLineOption
-  ( parse
+  ( class DefsToVals
+  , parse
   ) where
 
 import Bouzuya.CommandLineOption.ObjectToRecord (class OptionRecordBuilder)
 import Bouzuya.CommandLineOption.ObjectToRecord as ObjectToRecord
-import Bouzuya.CommandLineOption.OptionDefinition (NamedOptionDefinition)
+import Bouzuya.CommandLineOption.OptionDefinition (NamedOptionDefinition, TypedOptionDefinition)
 import Bouzuya.CommandLineOption.OptionObject as OptionObject
 import Bouzuya.CommandLineOption.RecordToArray (class ArrayBuilder)
 import Bouzuya.CommandLineOption.RecordToArray as RecordToArray
 import Data.Either (Either, note)
 import Prelude (bind, pure)
-import Prim.RowList (class RowToList)
+import Prim.RowList (class RowToList, Cons, Nil, kind RowList)
+import Type.Row (class ListToRow)
 
 type Parsed r =
   { arguments :: Array String
   , options :: Record r
   }
 
-parse :: forall r1 r2 l1 l2
+class DefsToVals (l1 :: RowList) (l2 :: RowList) | l1 -> l2
+instance defsToValsCons :: DefsToVals t1 t2 => DefsToVals (Cons l (TypedOptionDefinition a) t1) (Cons l a t2)
+instance defsToValsNil :: DefsToVals Nil Nil
+
+parse :: forall r1 r2 l1 l2 l3
    . RowToList r1 l1
   => ArrayBuilder l1 () r1 NamedOptionDefinition
   => RowToList r2 l2
   => OptionRecordBuilder l2 () r2
+  => DefsToVals l1 l3
+  => ListToRow l3 r2
   => Record r1
   -> Array String
   -> Either String (Parsed r2)
